48382debdc870093400942ca074c615d39cd7b5c
SohelShahid117 <sohelshahid09@gmail.com>
2024-04-18 19:50:10 +0600

    28 min done part-3

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,52 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
-    <div className="hero bg-base-200">
+    <div className="hero  -mt-16">
       <div className="hero-content text-center">
-        <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Welcome to ByteBlaze</h1>
+        <div className="max-w-lg">
+          <h1 className="text-5xl font-bold">
+            Welcome to{" "}
+            <span className="text-5xl font-bold bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text bg-300% animate-gradient">
+              ByteBlaze
+            </span>{" "}
+          </h1>
+          {/* animated gradient txt --- https://www.andrealves.dev/blog/how-to-make-an-animated-gradient-text-with-tailwindcss/ */}
           <p className="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
             a id nisi.
           </p>
-          <button className="btn btn-primary">Read Blogs</button>
-          <button className="btn btn-primary">Bookmarks</button>
+
+          {/* <button className="btn btn-primary">Read Blogs</button>
+          <button className="btn btn-primary">Bookmarks</button> */}
+          <div className="flex gap-3 justify-center">
+            {/* <Link to='/blogs'>Read Blogs</Link>
+            <Link to='/bookmarks'>Bookmarks</Link> */}
+            {/* tailwind-css-btn-link :   https://devdojo.com/tailwindcss/buttons#_ */}
+
+            <Link
+              to="/blogs"
+              className="relative inline-block px-4 py-2 font-medium group"
+            >
+              <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-primary group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+              <span className="absolute inset-0 w-full h-full bg-white border-2 border-secondary group-hover:bg-primary"></span>
+              <span className="relative text-black group-hover:text-white">
+                Read Blogs
+              </span>
+            </Link>
+            <Link
+              to="/bookmarks"
+              className="relative inline-block px-4 py-2 font-medium group"
+            >
+              <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-primary group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+              <span className="absolute inset-0 w-full h-full bg-white border-2 border-secondary group-hover:bg-primary"></span>
+              <span className="relative text-black group-hover:text-white">
+                Bookmarks
+              </span>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,19 @@
-import React from "react";
-import Navbar from "../components/Navbar";
+// import React from "react";
+// import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
-
+import wave from "../assets/wave.svg";
+// import sky from "../assets/sky.svg";
 const Home = () => {
   return (
     <div>
       {/* <div className="h-16">
         <Navbar></Navbar>
       </div> */}
-      <div className="flex flex-col items-center justify-center min-h-[calc(100vh-116px)]">
+      <div className="flex relative flex-col items-center justify-center min-h-[calc(100vh-116px)]">
         <Hero></Hero>
+        <img className="absolute bottom-0 w-full" src={wave}></img>
+        {/* animated svg background --- https://loading.io/background */}
+        {/* <img className="absolute bottom-0 w-full" src={sky}></img> */}
       </div>
       {/* <h2>This is home page</h2> */}
     </div>
